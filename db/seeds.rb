272ed2bfc4e666edf0508dd90068105b6af4792e
Ethaning <ethanjcarter@hotmail.co.uk>
2021-01-21 00:55:37 +0000

    Add sample data in db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+exercise_durations = {
+  "Overhead Squat" => 15,
+  "Side Plank Clam" => 30,
+  "Push-Up to chair" => 45,
+  "Squat Hold" => 30,
+  "Toe Tap" => 20,
+  "Reverse Lunge" => 25,
+  "Lateral Walk" => 37,
+  "Dumbbell Swing" => 40,
+  "Forward Raise" => 55,
+  "Front Squat" => 43,
+  "Overhead Press" => 25,
+  "1min Cardio" => 60,
+  "Knee to Elbow" => 25,
+  "Star Jump" => 30,
+  "Step Up" => 45,
+}
+
+exercise_durations.each do |name, duration|
+  Exercise.create! name: name, duration: duration
+end
+
+expertises = %i(yoga fitness strength)
+
+expertises.each do |name|
+  Expertise.create!(name: name)
+end
