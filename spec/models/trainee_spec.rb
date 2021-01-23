a429f2a736c87d9640017c54ac250b1d330aa4e4
Ethaning <ethanjcarter@hotmail.co.uk>
2021-01-23 11:01:07 +0000

    Ensure trainee does not require an expertise

---
+++ spec/models/trainee_spec.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  allow_password_change  :boolean          default(FALSE)
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
+#  provider               :string           default("email"), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  tokens                 :json
+#  type                   :string
+#  uid                    :string           default(""), not null
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  expertise_id           :bigint
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_expertise_id          (expertise_id)
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (expertise_id => expertises.id)
+#
+require 'rails_helper'
+
+RSpec.describe Trainee, type: :model do
+  subject { build :trainee }
+
+  context "relations" do
+    it { should_not belong_to(:expertise) }
+  end
+
+  context "validations" do
+    it { should_not validate_presence_of(:expertise_id) }
+  end
+end
